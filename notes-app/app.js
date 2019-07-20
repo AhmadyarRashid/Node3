5c7468a4e211cc6646d883cd840461a239a4a194
Ahmad yar meo <ahmadyarmeo@gmail.com>
2019-07-21 01:29:20 +0500

    use yargs lib

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,39 @@
-const validator = require('validator');
-const getNotes = require('./notes.js');
-const chalk = require('chalk');
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs');
 
-const msg = getNotes()
-console.log(msg);
-console.log(validator.isEmail('ahmad@gmail.com'));
-console.log(chalk.green.inverse.bold('SUCCESS!'));
+//yargs.version('1.1.0');
+yargs.command({
+    command : 'add',
+    describe : 'Add a new Note',
+    handler : function(){
+        console.log('Add New Note !')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe : 'Remove a Note',
+    handler : function(){
+        console.log('Remove a Note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe : 'show list of notes',
+    handler : function(){
+        console.log('display a list of notes')
+    }
+})
+
+yargs.command({
+    command : 'read',
+    describe : 'Read a note',
+    handler : function(){
+        console.log('Read a Note')
+    }
+})
+
+console.log(yargs.argv);
+
