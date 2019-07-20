24102416ed686e144e32cbfb61b3a02b96f552eb
Ahmad yar meo <ahmadyarmeo@gmail.com>
2019-07-21 00:54:29 +0500

    Printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
 const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
+
 const msg = getNotes()
 console.log(msg);
 console.log(validator.isEmail('ahmad@gmail.com'));
+console.log(chalk.green.inverse.bold('SUCCESS!'));
